173de87b50c805c78594e230cf212e97fcfef13d
Tioreza Febrian <tiorezafk@yahoo.com>
2018-01-21 11:11:16 +0700

    error msg ejs

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var auth = require('./routes/admin/auth');
 var databases = require("./config/db");
 
 
@@ -14,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,8 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', auth);
 
 databases.mysql.sync({force: false}).then(() => {
   console.log('Database Ready');
